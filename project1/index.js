fb16d2b8fa62e7abc8e12080aba546b1f32fa07d
david <leachda12@gmail.com>
2022-11-04 02:59:07 -0400

    it's 3 am

--- project1/index.js
+++ project1/index.js
@@ -3,5 +3,24 @@ $('.head').mousemove(e => {
     e.target.style.setProperty('--ypos', `${e.pageY}px`);
     $('.glow').css('left', `${e.pageX}px`);
     $('.glow').css('top', `${e.pageY}px`);
+});
+
+$('.fake-power').hover(e => {
+    $('.fake-power img').fadeIn(500);
+}, e => {
+    $('.fake-power img').fadeOut(500);
+});
+
+// class="animate_animated animate__zoomOut"
+// for the button on click
+
+$('.fake-power').click(e => {
+    $('.light-box').addClass('let-there-be-light');
+    $('.guise').fadeOut(1000);
+    $('.head').fadeOut(2000);
+    $('.power').fadeOut(1000);
+    $('.fake-power').fadeOut(1000);
 })
+
+
 
